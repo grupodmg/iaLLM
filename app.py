90eebebe3bd134eb26a0d0f54a2514384cff29d6
grupodmg <luberfandino@unicomfacauca.edu.co>
2025-05-17 13:32:28 -0500

    Agregar integración de router AI y mejorar manejo de respuestas en la API

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import aiRouter
 
 app = FastAPI()
+app.include_router(aiRouter.router)
 
 @app.get("/")
 def index():
--- routers/aiRouter.py
+++ routers/aiRouter.py
@@ -4,11 +4,35 @@ from interfaces.chatinterfaces import InputMessage
 
 router = APIRouter()
 
-client = OpenAI(api_key="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",
+client = OpenAI(api_key="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",
                 base_url="https://openrouter.ai/api/v1")
 
-@app.post("/ai-chat")
+
+@router.post("/ai-chat")
 def aiChat(data: InputMessage):
     data = data.model_dump()
     print("Mensaje:"+ data["message"])
-    return {"status": "ok"} 
+
+    message= "Por favor respode de manera correcta y concisa a la siguiente pregunta: "
+
+    try:
+        completion = client.chat.completions.create(
+            model="cognitivecomputations/dolphin3.0-r1-mistral-24b:free",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Eres un asistente de IA que responde de forma corta preguntas en español."
+                },
+                {
+                    "role": "user",
+                    "content": message + "Responde a esta pregunta: " + data["message"]
+                }
+            ],
+        )
+        response_text = getattr(completion.choices[0].message, "content", "")
+        print("response: " + response_text)
+        return {"status": "ok", "response": response_text}
+    
+    except Exception as e:
+        print("Error en la API de OpenAI:", e)
+        return {"error": f"Error en la API de OpenAI: {str(e)}"}
