cbcfc2ec2a889b6b240c0b128e855ae8ba8854d5
grupodmg <luberfandino@unicomfacauca.edu.co>
2025-05-17 10:59:28 -0500

    Agregar script LLM para interacción con OpenAI y respuesta a preguntas en español

--- LLM.py
+++ LLM.py
@@ -4,8 +4,8 @@ client = OpenAI(api_key="sk-or-v1-ec586c008eab5f8823a35e807c789cd11f258bc5a279dc
                 base_url="https://openrouter.ai/api/v1")
 
 
-messages = input("Cual es tu pregunta") 
-prompt = ("Por favor responde de manera clara y sin símbolos innecesarios. Evita usar otros idiomas que no sean el español y escribe una respuesta concisa. Pregunta del usuario: dame la formla para hacer arroz")
+message = input("Cual es tu pregunta: ") 
+prompt = f"Por favor responde de manera clara y sin símbolos innecesarios. Evita usar otros idiomas que no sean el español y escribe una respuesta concisa. Pregunta del usuario: {message}"
         
 completion = client.chat.completions.create(
     model="cognitivecomputations/dolphin3.0-r1-mistral-24b:free",
